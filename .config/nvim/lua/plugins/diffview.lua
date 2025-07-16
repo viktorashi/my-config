659411b315a8c1aacbc8d1c304db36014d9d2c5e
viktorashi <ioanvictorstan@gmail.com>
2025-07-17 01:11:02 +0300

    diffview misto

---
+++ .config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "sindrets/diffview.nvim",
+    keys = {
+      {
+        "<leader>gd",
+        "<cmd>:DiffviewOpen<CR>",
+        mode = { "n" },
+        desc = "Diffview",
+      },
+    },
+  },
+}
---
+++ .config/nvim/lua/plugins/neogit.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua", -- optional
+      "echasnovski/mini.pick", -- optional
+      "folke/snacks.nvim", -- optional
+    },
+    keys = {
+      {
+        "<leader>gn",
+        "<cmd>lua require('neogit').open({kind = 'floating'})<CR>",
+        mode = { "n" },
+        desc = "Neogit normal gen",
+      },
+    },
+  },
+}
