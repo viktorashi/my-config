4ba1fa41d2d6ca40c482aaced9d658cc37aa5558
viktorashi <ioanvictorstan@gmail.com>
2025-07-17 00:53:48 +0300

    nu era nevoie in ambele parti de asta

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,13 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set(
-  -- vezi ca tre sa apesi gen frt repede ca sa mearga ca altfel se deschide UI menu
-  "n",
-  "<leader>u",
-  require("undotree").toggle,
-  { noremap = true, silent = true }
-)
+-- vim.keymap.set(
+--   -- vezi ca tre sa apesi gen frt repede ca sa mearga ca altfel se deschide UI menu
+--   "n",
+--   "<leader>u",
+--   require("undotree").toggle,
+--   { noremap = true, silent = true }
+-- )
 
 -- sper ca merge pe windows panarama
 vim.keymap.set("n", "<leader><delete>", function()
