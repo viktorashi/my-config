cb2847c768f275cf4ac0932e92e1bcb574b3d74f
viktorashi <ioanvictorstan@gmail.com>
2025-08-04 19:20:30 +0300

    kkt ca mi-a stricat remoturile

--- docs/cloning-setup.sh
+++ docs/cloning-setup.sh
@@ -17,7 +17,7 @@ alias config='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias config='conf'
 
 conf config --local status.showUntrackedFiles no #only account for the files you specifically mention
-conf checkout
+conf checkout main
 conf config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
 conf fetch
 conf branch --set-upstream-to=origin/main
@@ -25,6 +25,7 @@ conf switch mac
 conf branch --set-upstream-to=origin/mac
 conf switch windows10
 conf branch --set-upstream-to=origin/windows10
+conf checkout main
 
 cd ~/docs/
 #no hackerino
