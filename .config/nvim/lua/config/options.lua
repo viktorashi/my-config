8348ac03d6963e2e4cf8b324c4081de4d36d2725
viktorashi <viktorashi@Viks-MacBook-Air.local>
2025-10-18 21:13:11 +0300

    cv update de la ei

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,6 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr =
+  "v:lua.LazyVim.treesitter.foldexpr()"
+
 -- Neovide font configuration
 if vim.g.neovide then
   -- UI scaling
