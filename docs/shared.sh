a6aed9cd52030f00bbe47b9db91d2d808ecb0a35
viktorashi <ioanvictorstan@gmail.com>
2025-11-09 20:23:15 +0200

    clear inaitne de radian

--- docs/shared.sh
+++ docs/shared.sh
@@ -88,7 +88,6 @@ alias ct='clear && cargo test'
 alias cb='clear && cargo build'
 
 alias r='clear && radian  --ask-save --save --restore-data --debug'
-
 #alias fr='flutter run'
 #acm de cand cu zoxide nu prea mai e nevoie lmao
 #alias licenta='cd ~/Documents/toate-de-la-faculta-trecut/licenta/texuri/bachelor-thesis-repo/bachelor-thesis'
