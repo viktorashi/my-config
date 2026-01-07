5920df9b4be39fb7516dae0d9d13d9761843e03a
vic-stan <i.stan@stratec.com>
2026-01-07 20:27:40 +0200

    Fuzzy finder din ăla mișto

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -249,3 +249,13 @@ vim.keymap.set("n", "<leader>ff", function()
 end, {
   desc = "Find Text (Grep including hidden files)",
 })
+
+-- Add this to your keymaps or init.lua
+vim.keymap.set("n", "<leader>sf", function()
+  require("telescope.builtin").grep_string({
+    short_path = true,
+    word_match = "-w",
+    only_sort_text = true,
+    search = "",
+  })
+end, { desc = "Fuzzy Grep (Content)" })
