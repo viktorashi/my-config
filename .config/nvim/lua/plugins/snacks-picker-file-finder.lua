665fc5f8be0a0fc3a622d33844af59629d79f08a
vic-stan <i.stan@stratec.com>
2026-02-13 19:36:32 +0200

    so my snacks knows where to find sqlite3

--- .config/nvim/lua/plugins/snacks-picker-file-finder.lua
+++ .config/nvim/lua/plugins/snacks-picker-file-finder.lua
@@ -3,6 +3,9 @@ return {
     "folke/snacks.nvim",
     opts = {
       picker = {
+        db = {
+          sqlite3_path = "/lib/x86_64-linux-gnu/libsqlite3.so.0",
+        },
         hidden = true, -- for hidden files
         ignored = true, -- for .gitignore files
         sources = {
