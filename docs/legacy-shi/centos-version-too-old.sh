90a8618a15e89edd52a9a9942d114a30db7459ea
viktorashi <ioanvictorstan@gmail.com>
2026-03-31 00:04:29 +0300

    uncomment dis

--- docs/legacy-shi/centos-version-too-old.sh
+++ docs/legacy-shi/centos-version-too-old.sh
@@ -8,7 +8,7 @@ sudo sed -i 's/^#baseurl=/baseurl=/g' /etc/yum.repos.d/CentOS-Base.repo
 sudo sed -i 's/mirror.centos.org\/centos\/$releasever/archive.kernel.org\/centos-vault\/5.11/g' /etc/yum.repos.d/CentOS-Base.repo
 
 #acum poti sa dai refresh la repourile astea
-# sudo yum clean all
+sudo yum clean all
 
 # sidai install la ce vrei tu gen
-# sudo yum install gcc hexedit nasm binutils strace ltrace
+sudo yum install gcc hexedit nasm binutils strace ltrace
