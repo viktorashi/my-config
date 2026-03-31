f825849679d98eda32bbf15c6ac7f8bddd8c8842
istan <i.stan@stratec.com>
2026-03-31 10:29:36 +0300

    Just being able to wt and im without exe

---
+++ docs/linkables/no-exe-wrapper-scripts.sh
@@ -0,0 +1,2 @@
+echo '#!/bin/bash' >~/.local/bin/wt && echo 'wt.exe "$@"' >>~/.local/bin/wt && chmod +x ~/.local/bin/wt
+echo '#!/bin/bash' >~/.local/bin/im && echo 'im.exe "$@"' >>~/.local/bin/im && chmod +x ~/.local/bin/im
